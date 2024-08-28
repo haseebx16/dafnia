37205de04d6981e14f7cd616629e7ea20d3332f4
Anonymous <haseebhamid554@gmail.com>
2024-08-28 06:20:10 -0700

    Dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,34 +3,39 @@ import Link from 'next/link';
 import { FaHome, FaUser } from "react-icons/fa";
 import { GoGraph } from "react-icons/go";
 import { IoMdSettings } from "react-icons/io";
+import { PiCashRegisterLight } from "react-icons/pi";
+import { IoIosCash } from "react-icons/io";
+import { SiCashapp } from "react-icons/si";
+import { BsCashCoin } from "react-icons/bs";
+import { LiaCashRegisterSolid } from "react-icons/lia";
 
 export default function page() {
   return (
     <div className="flex min-h-screen">
-          <aside className="w-72 bg-white text-white flex flex-col">
-              <div className='flex justify-center items-center'>
-                  <img src="/logo-dafnia.png" className='w-40 h-36'/>
-                </div>
+      <aside className="w-80 bg-white text-white flex flex-col">
+        <div className='flex justify-center items-center'>
+          <img src="/logo-dafnia.png" className='w-40 h-36'/>
+        </div>
         <nav className="flex-1 px-4 mt-6 space-y-4">
           <a href="#" className="flex py-2 px-4 rounded transition duration-200 bg-blue-500 text-white">
-                      <FaHome />&nbsp;&nbsp;{" "}Home
+            <FaHome />&nbsp;&nbsp;{" "}Home
           </a>
           <a href="#" className="flex py-2 px-4 text-black  rounded  transition duration-200 hover:bg-blue-500 hover:text-white">
             <GoGraph/>&nbsp;&nbsp;{" "}Analytics
           </a>
           <a href="#" className="flex py-2 px-4 text-black  rounded transition duration-200 hover:bg-blue-500 hover:text-white">
-           <FaUser/> &nbsp;&nbsp;{" "}Customers
+            <FaUser/> &nbsp;&nbsp;{" "}Customers
           </a>
           <a href="#" className="flex py-2 px-4 text-black  rounded transition duration-200 hover:bg-blue-500 hover:text-white">
             <IoMdSettings/>&nbsp;&nbsp;{" "}Settings
           </a>
         </nav>
-              <div className="p-6">
-                  <Link href="/">
-          <button className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            Logout
-                      </button>
-                      </Link>
+        <div className="p-6">
+          <Link href="/">
+            <button className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+              Logout
+            </button>
+          </Link>
         </div>
       </aside>
 
@@ -50,44 +55,84 @@ export default function page() {
         </header>
 
         <main className="flex-1 p-6 bg-gray-100">
-          <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6">
-            <div className="bg-white p-6 rounded-lg shadow-md flex items-center space-x-4">
-              <div className="bg-blue-500 h-12 w-12 rounded-full flex items-center justify-center text-white">
-                <i className="fas fa-chart-line"></i>
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+          <div className="bg-white p-6 rounded-lg shadow-md text-center">
+              <p className="text-gray-600 font-medium text-left  text-xl mb-4">Open AP Invoices</p>
+              <div className='flex justify-center items-center flex-wrap'>
+                <p className="text-gray-600 font-bold text-2xl  ml-12">38</p>
+                <p className='text-gray-600 font-bold text-2xl  ml-16'>150M</p>
+               <p className="bg-blue-300 text-blue-500 text-xs p-1 rounded-md ml-2 font-bold">SAR</p>
               </div>
-              <div>
-                <h2 className="text-lg font-semibold">Sales</h2>
-                <p className="text-gray-600">$24,000</p>
+              <div className='flex justify-center'>
+                <p className='text-gray-600 font-medium mr-20'>Total</p>
+                <p className='text-gray-600 font-medium'>Value</p>
+              </div>
+              <div className='flex justify-center items-center'>
+
               </div>
             </div>
 
-            <div className="bg-white p-6 rounded-lg shadow-md flex items-center space-x-4">
-              <div className="bg-green-500 h-12 w-12 rounded-full flex items-center justify-center text-white">
-                <i className="fas fa-user-friends"></i>
+            <div className="bg-white p-6 rounded-lg shadow-md text-center">
+              <p className="text-gray-600 font-medium text-left  text-xl mb-4">Pending AP Invoices</p>
+              <div className='flex justify-center items-center flex-wrap'>
+                <p className="text-gray-600 font-bold text-2xl  ml-12">38</p>
+                <p className='text-gray-600 font-bold text-2xl  ml-16'>150M</p>
+               <p className="bg-blue-300 text-blue-500 text-xs p-1 rounded-md ml-2 font-bold">SAR</p>
               </div>
-              <div>
-                <h2 className="text-lg font-semibold">Customers</h2>
-                <p className="text-gray-600">1,200</p>
+              <div className='flex justify-center'>
+                <p className='text-gray-600 font-medium mr-20'>Total</p>
+                <p className='text-gray-600 font-medium'>Value</p>
+              </div>
+              <div className='flex justify-center items-center'>
+
               </div>
             </div>
 
-            <div className="bg-white p-6 rounded-lg shadow-md flex items-center space-x-4">
-              <div className="bg-yellow-500 h-12 w-12 rounded-full flex items-center justify-center text-white">
-                <i className="fas fa-box"></i>
+            <div className="bg-white p-6 rounded-lg shadow-md text-center">
+              <p className="text-gray-600 font-medium text-left  text-xl mb-4">Approved AP Invoices</p>
+              <div className='flex justify-center items-center flex-wrap'>
+                <p className="text-gray-600 font-bold text-2xl  ml-12">38</p>
+                <p className='text-gray-600 font-bold text-2xl  ml-16'>150M</p>
+               <p className="bg-blue-300 text-blue-500 text-xs p-1 rounded-md ml-2 font-bold">SAR</p>
               </div>
-              <div>
-                <h2 className="text-lg font-semibold">Products</h2>
-                <p className="text-gray-600">300</p>
+              <div className='flex justify-center'>
+                <p className='text-gray-600 font-medium mr-20'>Total</p>
+                <p className='text-gray-600 font-medium'>Value</p>
+              </div>
+              <div className='flex justify-center items-center'>
+
               </div>
             </div>
 
-            <div className="bg-white p-6 rounded-lg shadow-md flex items-center space-x-4">
-              <div className="bg-red-500 h-12 w-12 rounded-full flex items-center justify-center text-white">
-                <i className="fas fa-chart-pie"></i>
+            <div className="bg-white p-6 rounded-lg shadow-md text-center">
+              <p className="text-gray-600 font-medium text-left  text-xl mb-4">Open Shipments</p>
+              <div className='flex justify-center items-center flex-wrap'>
+                <p className="text-gray-600 font-bold text-2xl  ml-12">38</p>
+                <p className='text-gray-600 font-bold text-2xl  ml-16'>150M</p>
+               <p className="bg-blue-300 text-blue-500 text-xs p-1 rounded-md ml-2 font-bold">SAR</p>
               </div>
-              <div>
-                <h2 className="text-lg font-semibold">Analytics</h2>
-                <p className="text-gray-600">75%</p>
+              <div className='flex justify-center'>
+                <p className='text-gray-600 font-medium mr-20'>Total</p>
+                <p className='text-gray-600 font-medium'>Value</p>
+              </div>
+              <div className='flex justify-center items-center'>
+
+              </div>
+            </div>
+
+            <div className="bg-white p-6 rounded-lg shadow-md text-center">
+              <p className="text-gray-600 font-medium text-left  text-xl mb-4">Open DP</p>
+              <div className='flex justify-center items-center flex-wrap'>
+                <p className="text-gray-600 font-bold text-2xl  ml-12">38</p>
+                <p className='text-gray-600 font-bold text-2xl  ml-16'>150M</p>
+               <p className="bg-blue-300 text-blue-500 text-xs p-1 rounded-md ml-2 font-bold">SAR</p>
+              </div>
+              <div className='flex justify-center'>
+                <p className='text-gray-600 font-medium mr-20'>Total</p>
+                <p className='text-gray-600 font-medium'>Value</p>
+              </div>
+              <div className='flex justify-center items-center'>
+
               </div>
             </div>
           </div>
