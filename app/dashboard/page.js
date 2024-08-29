4f0bb254f46f249d4f196583af3f74fb4c9323fc
Nuxhat <nuxhat.syed@gmail.com>
2024-08-29 12:25:23 +0500

    dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -12,47 +12,39 @@ import { LiaCashRegisterSolid } from "react-icons/lia";
 export default function page() {
   return (
     <div className="flex min-h-screen">
-      <aside className="w-80 bg-white text-white flex flex-col">
+      <aside className="w-80 bg-white text-white flex flex-col bg-gradient-to-b from-white from-20% via-blue-300 to-sky-800">
         <div className='flex justify-center items-center'>
-          <img src="/logo-dafnia.png" className='w-40 h-36'/>
+          <img src="/logo-dafnia.png" className='w-auto h-36 mt-6'/>
         </div>
         <nav className="flex-1 px-4 mt-6 space-y-4">
-          <a href="#" className="flex py-2 px-4 rounded transition duration-200 bg-blue-500 text-white">
+          <a href="#" className="flex py-2 px-4 rounded transition duration-200 bg-sky-600 text-white">
             <FaHome />&nbsp;&nbsp;{" "}Home
           </a>
-          <a href="#" className="flex py-2 px-4 text-black  rounded  transition duration-200 hover:bg-blue-500 hover:text-white">
-            <GoGraph/>&nbsp;&nbsp;{" "}Analytics
+          <a href="#" className="flex py-2 px-4 text-black  rounded  transition duration-200 hover:bg-sky-600 hover:text-white">
+            <GoGraph/>&nbsp;&nbsp;{" "}Company
           </a>
-          <a href="#" className="flex py-2 px-4 text-black  rounded transition duration-200 hover:bg-blue-500 hover:text-white">
-            <FaUser/> &nbsp;&nbsp;{" "}Customers
+          <a href="#" className="flex py-2 px-4 text-black  rounded transition duration-200 hover:bg-sky-600 hover:text-white">
+            <FaUser/> &nbsp;&nbsp;{" "}Users
           </a>
-          <a href="#" className="flex py-2 px-4 text-black  rounded transition duration-200 hover:bg-blue-500 hover:text-white">
+          <a href="#" className="flex py-2 px-4 text-black  rounded transition duration-200 hover:bg-sky-600 hover:text-white">
             <IoMdSettings/>&nbsp;&nbsp;{" "}Settings
           </a>
         </nav>
         <div className="p-6">
           <Link href="/">
-            <button className="w-full px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            {/* <button className="w-full px-4 py-2 bg-sky-700 text-white rounded hover:bg-sky-600">
               Logout
-            </button>
+            </button> */}
           </Link>
         </div>
       </aside>
 
       <div className="flex-1 flex flex-col">
-        <header className="flex items-center justify-between p-6 bg-white border border-gray-100">
+        <header className="flex items-center justify-between p-6 bg-gray-100 border border-gray-100">
           <h1 className="text-3xl font-semibold">Main Dashboard</h1>
-          <div className="flex items-center space-x-4">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-              Notifications
-            </button>
-          </div>
+          
         </header>
+          <hr className=" border-gray-700 w-full"/>
 
         <main className="flex-1 p-6 bg-gray-100">
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
