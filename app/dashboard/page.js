422413e7f41611d16d3740c6594f962190944257
Billyoo00 <146122962+Billyoo00@users.noreply.github.com>
2024-08-29 11:28:03 +0500

    dashboard shadow

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -56,8 +56,8 @@ export default function page() {
 
         <main className="flex-1 p-6 bg-gray-100">
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-           <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group">
-           <p className="font-bold shadow-gray-500 shadow-sm bg-sky-600 text-white p-2 rounded-md text-xl mb-4 text-center group-hover:bg-white group-hover:text-black">
+           <div className="bg-white p-6 rounded-lg text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group shadow-gray-400 shadow-lg" >
+           <p className="font-bold shadow-gray-500 shadow-sm bg-sky-600 text-white p-2 rounded-md text-xl mb-4 text-center group-hover:bg-white group-hover:text-black ">
               Open AP Invoices
             </p>
               <div className="flex justify-center items-center flex-wrap">
@@ -74,7 +74,7 @@ export default function page() {
             </div>
 
 
-            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group">
+            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group shadow-gray-400 shadow-lg">
               <p className="font-bold shadow-gray-500 shadow-sm bg-sky-600 text-white p-2 rounded-md text-xl mb-4 text-center group-hover:bg-white group-hover:text-black">
                 Pending AP Invoices
               </p>
@@ -91,7 +91,7 @@ export default function page() {
               </div>
             </div>
 
-            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group">
+            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group shadow-gray-400 shadow-lg">
               <p className="font-bold shadow-gray-500 shadow-sm bg-sky-600 text-white p-2 rounded-md text-xl mb-4 text-center group-hover:bg-white group-hover:text-black">
                 Approved AP Invoices
               </p>
@@ -108,7 +108,7 @@ export default function page() {
               </div>
             </div>
 
-            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group">
+            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group shadow-gray-400 shadow-lg">
               <p className="font-bold shadow-gray-500 shadow-sm bg-sky-600 text-white p-2 rounded-md text-xl mb-4 text-center group-hover:bg-white group-hover:text-black">
                 Open Shipments
               </p>
@@ -125,7 +125,7 @@ export default function page() {
               </div>
             </div>
 
-            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group">
+            <div className="bg-white p-6 rounded-lg shadow-md text-center hover:cursor-pointer hover:bg-gradient-to-r from-blue-400 to-sky-900 hover:text-white duration-400 group shadow-gray-400 shadow-lg">
               <p className="font-bold shadow-gray-500 shadow-sm bg-sky-600 text-white p-2 rounded-md text-xl mb-4 text-center group-hover:bg-white group-hover:text-black">
                 Open DP
               </p>
