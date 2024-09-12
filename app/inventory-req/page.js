6809f0979aa3bcd363dd971151aa68904e914cdc
ZubairAhmed90 <106171902+ZubairAhmed90@users.noreply.github.com>
2024-09-12 16:29:40 +0500

    updated

--- app/inventory-req/page.js
+++ app/inventory-req/page.js
@@ -2,6 +2,7 @@
 import Layout from '../components/Layout/Layout';
 import React, { useState } from 'react';
 import { RiDeleteBin6Line } from "react-icons/ri";
+import RoundedField from '../components/text-field/field';
 
 import {
     Grid,
@@ -63,14 +64,22 @@ function Page() {
       <div className=''>
         {/* Title Section */}
         <div className="mt-4">
-        <p className="text-2xl font-bold text-black mt-7">Inventory Transfer Request</p>
+        <p className="text-2xl ml-4  font-bold text-black mt-7">Inventory Transfer Request</p>
         <hr className="border-t-2 border-gray-700 mt-5 " />
       </div>
 
-        <div className="grid grid-cols-2 gap-96 bg-white p-6">
+        <div className="grid grid-cols-2 gap-96 bg-white p-6 rounded-lg mt-2">
         <div className="space-y-2 ml-1">
             {/* Business Partner Dropdown */}
-            <div className="flex items-center mt-2 bg-blue-50 py-2 px-4   rounded-md">
+            <h1 className='text-xl font-bold'>Sending Site:</h1>
+           
+              
+              <RoundedField id="company" name="company" label="Ship-From Site ID" type="text" className='bg-bl' status='' />
+         
+            <div className="grid grid-cols-3 gap-96 bg-white p-6"></div>
+              <h1 className='text-xl font-bold'>General:</h1>
+            {/* <div className="flex items-center mt-2 bg-blue-50 py-2 px-4   rounded-md">
+              
               <label className="block text-gray-700 text-sm w-32">Business Partner: </label>
               <select
                 name="businessPartner"
@@ -83,151 +92,66 @@ function Page() {
                 <option value="Partner2">Partner 2</option>
                 <option value="Partner3">Partner 3</option>
               </select>
-            </div>
-
-            {/* Name Dropdown */}
-            <div className="flex items-center mt-2 bg-blue-50 py-2 px-4 rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">Name: </label>
-              <select
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              >
-                <option value="">Select Name</option>
-                <option value="Name1">Name 1</option>
-                <option value="Name2">Name 2</option>
-                <option value="Name3">Name 3</option>
-              </select>
-            </div>
-
-            {/* Contact Person Dropdown */}
-            <div className="flex items-center mt-2 bg-blue-50 py-2 px-4 rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">Contact Person: </label>
-              <select
-                name="contactPerson"
-                value={formData.contactPerson}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              >
-                <option value="">Select Contact Person</option>
-                <option value="Person1">Person 1</option>
-                <option value="Person2">Person 2</option>
-                <option value="Person3">Person 3</option>
-              </select>
-            </div>
-
-            {/* Ship To Dropdown */}
-            <div className="flex items-center mt-2 bg-blue-50 py-2 px-4 rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">Ship To: </label>
-              <select
-                name="shipTo"
-                value={formData.shipTo}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              >
-                <option value="">Select Location</option>
-                <option value="Location1">Location 1</option>
-                <option value="Location2">Location 2</option>
-                <option value="Location3">Location 3</option>
-              </select>
-            </div>
-          </div>
-          {/* Left Column */}
-          <div className="space-y-1 flex flex-col items-end mr-4 justify-end">
-            {/* No. Dropdown */}
-            <div className="flex items-center bg-blue-50 py-2 px-4   rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">No: </label>
-              <select
-                name="no"
-                value={formData.no}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              >
-                <option value="">Select No</option>
-                <option value="Primary">Primary</option>
-                <option value="Secondary">Secondary</option>
-              </select>
-            </div>
-
-            {/* Status Input */}
-            <div className="flex items-center bg-blue-50 py-2 px-4   rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">Status: </label>
-              <input
-                type="text"
-                name="status"
-                value={formData.status}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              />
-            </div>
-
-            {/* Posting Date */}
-            <div className="flex items-center bg-blue-50 py-2 px-4   rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">Posting Date: </label>
-              <input
-                type="date"
-                name="postingDate"
-                value={formData.postingDate}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              />
-            </div>
-
-            {/* Due Date */}
-            <div className="flex items-center bg-blue-50 py-2 px-4   rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">Due Date: </label>
-              <input
-                type="date"
-                name="dueDate"
-                value={formData.dueDate}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              />
-            </div>
-
-            {/* Document Date */}
-            <div className="flex items-center bg-blue-50 py-2 px-4   rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">Document Date: </label>
-              <input
-                type="date"
-                name="documentDate"
-                value={formData.documentDate}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              />
-            </div>
+            </div> */}
+      <div className="space-y-1 flex items-start">
+    {/* Status Input */}
+    <div className="flex items-center bg-blue-50 py-2 px-4 rounded-md">
+      <label className="block text-gray-700 mb-1 text-sm w-32">Ship-To Site ID: </label>
+      <input
+        type="text"
+        name="status"
+        value={formData.status}
+        onChange={handleChange}
+        className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
+      />
+    </div>
+    <div className="flex items-center bg-blue-50 py-2 px-4 rounded-md">
+      <label className="block text-gray-700 mb-1 text-sm w-32">Ship-To Site ID: </label>
+      <input
+        type="text"
+        name="status"
+        value={formData.status}
+        onChange={handleChange}
+        className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
+      />
+    </div>
+    <div className="flex items-center bg-blue-50 py-2 px-4 rounded-md">
+      <label className="block text-gray-700 mb-1 text-sm w-32">Ship-To Site ID: </label>
+      <input
+        type="text"
+        name="status"
+        value={formData.status}
+        onChange={handleChange}
+        className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
+      />
+    </div>
+  </div>
             
-            {/* From Warehouse Dropdown */}
-            <div className="flex items-center bg-blue-50 py-2 px-4   rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">From Warehouse: </label>
-              <select
-                name="fromWarehouse"
-                value={formData.fromWarehouse}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              >
-                <option value="">Select Warehouse</option>
-                <option value="WHS-0001">WHS-0001</option>
-                <option value="WHS-0002">WHS-0002</option>
-              </select>
-            </div>
+           
+           
 
-            {/* To Warehouse Dropdown */}
-            <div className="flex items-center bg-blue-50 py-2 px-4   rounded-md">
-              <label className="block text-gray-700 mb-1 text-sm w-32">To Warehouse: </label>
-              <select
-                name="toWarehouse"
-                value={formData.toWarehouse}
-                onChange={handleChange}
-                className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-              >
-                <option value="">Select Warehouse</option>
-                <option value="WHS-0001">WHS-0001</option>
-                <option value="WHS-0002">WHS-0002</option>
-              </select>
-            </div>
+           
           </div>
+          {/* right Column */}
+       {/* Right Column */}
+<div className="flex flex-col">
+  <h1 className='font-bold text-xl mb-2'>Receiving Site:</h1>
+
+  <div className="space-y-1 flex flex-col items-start">
+    {/* Status Input */}
+    <div className="flex items-center bg-blue-50 py-2 px-4 rounded-md">
+      <label className="block text-gray-700 mb-1 text-sm w-32">Ship-To Site ID: </label>
+      <input
+        type="text"
+        name="status"
+        value={formData.status}
+        onChange={handleChange}
+        className="w-72 border border-gray-300 p-1 rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
+      />
+    </div>
+  </div>
+</div>
+
         </div>
 
         {/* Items Table */}
